639f3b203e862c873a723ace8faf95f09ebe29a8
Gerard Kanters <gkanters@gmail.com>
2016-03-03 17:15:09 +0100

    Update settings.php
    Move disable htmlawed to allow javascript to start (doesn't work here)

--- views/default/plugins/welcome/settings.php
+++ views/default/plugins/welcome/settings.php
@@ -2,10 +2,6 @@
 /**
  * Plugin settings
  */
-
-if (elgg_is_admin_logged_in()) {
-    elgg_unregister_plugin_hook_handler('validate', 'input', 'htmlawed_filter_tags');
-}
 $welcome_input = $vars['entity']->welcome_adwords;
 
 echo elgg_echo('welcome:welcome_adwords');
